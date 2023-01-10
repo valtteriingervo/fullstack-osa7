fff19b707f79cd02a38fe26aa3c156edcb60a771
Valtteri Ingervo <valtteri.ingervo@aalto.fi>
2023-01-10 15:23:14 +0200

    7.7

--- country-hook/src/App.js
+++ country-hook/src/App.js
@@ -15,10 +15,25 @@ const useField = (type) => {
   }
 }
 
+// CUSTOM HOOK TO BE IMPLEMENTED
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  // The useEffect hook should be run again when the name of the country changes
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+        .then(response => {
+          setCountry(response.data[0])
+        })
+        .catch(error => {
+          // In case of error (so in practice country is misspelled),
+          // set country to null to show the 'country not found ..' text 
+          setCountry(null)
+        })
+    }
+  }, [name])
 
   return country
 }
@@ -31,11 +46,11 @@ const Country = ({ country }) => {
   return (
     <div>
       <h3>{country.name.common}</h3>
-      <div>population {country.population}</div> 
+      <div>population {country.population}</div>
       <div>capital {country.capital}</div>
-      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
+      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`} />
     </div>
-  )  
+  )
 }
 
 const App = () => {
